0cbf4365f8e4f109e6af31955860a54ca9af8adc
EugeneBachura <eugenebachura@gmail.com>
2022-09-25 08:26:10 +0200

    general and profile layouts
    layout general dashboard, layout and routers profile page

--- routes/web.php
+++ routes/web.php
@@ -25,23 +25,41 @@ Route::get('/', function () {
         'laravelVersion' => Application::VERSION,
         'phpVersion' => PHP_VERSION,
     ]);
-});
+})->name('main');
 
+/* Main dashboard */
 Route::get('/dashboard', function () {
-    $user = Auth::user();
-    $user_id = $user->id;
-    $role = $user->getRoleNames();
-    $user_data = null;
-    if ($user->hasRole('candidate')) {
-        $user_data = Candidate::where('user_id', $user_id)->first();
-    };
-    if ($user->hasRole('recruiter')) {
-        $user_data = Recruiter::where('user_id', $user_id)->first();
-    };
-    return view('dashboard/main', [
-        'user' => $user_data->only(['first_name', 'last_name', 'firm_name', 'position', 'telephone', 'user_email']),
-        'role' => $role,
-    ]);
+    /* Candidate */
+    if (Auth::user()->hasRole('candidate')) {
+        $user_data = Candidate::where('user_id', Auth::user()->id)->first();
+        return view('dashboard/main', [
+            'user' => $user_data->only(['first_name', 'last_name', 'user_email']),
+        ]);
+    }
+    /* Recruiter */
+    if (Auth::user()->hasRole('recruiter')) {
+        $user_data = Recruiter::where('user_id', Auth::user()->id)->first();
+        return view('dashboard/main', [
+            'user' => $user_data->only(['first_name', 'last_name', 'user_email']),
+        ]);
+    }
 })->middleware(['auth', 'verified'])->name('dashboard');
+
+/* Profile */
+Route::get('/profile', function () {
+    /* Candidate */
+    if (Auth::user()->hasRole('candidate')) {
+        $user_data = Candidate::where('user_id', Auth::user()->id)->first();
+        return view('dashboard/profile', [
+            'user' => $user_data->only(['first_name', 'last_name', 'date_of_birth', 'interests', 'education', 'skills', 'telephone', 'cv_link', 'user_email', 'updated_at']),
+        ]);
+    }
+    /* Recruiter */
+    if (Auth::user()->hasRole('recruiter')) {
+        $user_data = Recruiter::where('user_id', Auth::user()->id)->first();
+        return view('dashboard/profile', []);
+    }
+})->middleware(['auth', 'verified'])->name('profile');
+
 
 require __DIR__ . '/auth.php';
